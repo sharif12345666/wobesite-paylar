021ec416537087c12739931d29d588c4c7c09fc2
sharif12345666 <sharifgazi695@gmail.com>
2022-03-02 00:27:58 +0600

    thard commit add

--- apps.js
+++ apps.js
@@ -1,9 +1,13 @@
 const allPlayers = () => {
       const searchValue = document.getElementById('input-box').value;
       const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`
-      console.log(url)
+      // console.log(url)
       fetch(url)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => showPlayers(data.player))
       console.log(searchValue)
+
+}
+const showPlayers = (playar) => {
+      console.log(playar)
 }
